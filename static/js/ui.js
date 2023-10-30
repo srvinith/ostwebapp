6a8dfa98b6cf2fca9fa76aa6c9ba7b6f15815914
srvinith <r36041595@gmail.com>
2023-10-30 17:29:50 +0530

    devices chages

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,12 +1,35 @@
-function navshow(){
-   
+function navshow() {
+
     const centernav = document.getElementById('centernav');
-    if(centernav.style.display == 'none'){
-        
-        centernav.style.display='block'
+    if (centernav.style.display == 'none') {
+
+        centernav.style.display = 'block'
     }
-    else{
-        centernav.style.display='block'
+    else {
+        centernav.style.display = 'block'
     }
 }
 
+// Get references to the buttons
+const toggleButtons = document.querySelectorAll('.toggleButton');
+// const onlights = document.querySelector('.onlights');
+// const offlights = document.querySelector('.offlights');
+
+// Add click event listeners to each button
+toggleButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const currentValue = button.getAttribute('value');
+    
+    // Toggle the value for the clicked button
+    button.setAttribute('value', currentValue === 'true' ? 'false' : 'true');
+
+    // Update the view based on the value
+    if (currentValue === 'true') {
+      button.classList.remove('true-color');
+   
+    } else {
+      button.classList.add('true-color');
+      
+    }
+  });
+});
